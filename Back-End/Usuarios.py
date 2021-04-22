370c7da78a6ab427fddfa1e3d33ba43f6dfd45c8
Bryan <2780289260101@ingenieria.usac.edu.gt>
2021-04-22 02:14:52 -0600

    clasificacion tipo usuario

--- Back-End/Usuarios.py
+++ Back-End/Usuarios.py
@@ -9,8 +9,21 @@ class Usuario:
         self.password = password
         self.telefono = telefono
 
-class Doctor(Usuario):
-    def __init__(self,tipo,nombre,apellido,fecha,genero,usuario,password,especialidad,telefono):
-        super().__init__(lado,lado)
+class Especialidad:
+    def __init__(self,usuario,especialidad):
         self.usuario = usuario
         self.especialidad = especialidad
+
+class Doctor(Usuario):
+    def __init__(self,nombre,apellido,fecha,genero,usuario,password,especialidad,telefono):
+        super().__init__(nombre,apellido,fecha,genero,usuario,password,especialidad,telefono)
+        self.usuario = usuario
+        self.especialidad = especialidad
+
+class Paciente(Usuario):
+    def __init__(self,nombre,apellido,fecha,genero,usuario,password,especialidad,telefono):
+        super().__init__(nombre,apellido,fecha,genero,usuario,password,especialidad,telefono)
+
+class Enfermero(Usuario):
+    def __init__(self,nombre,apellido,fecha,genero,usuario,password,especialidad,telefono):
+        super().__init__(nombre,apellido,fecha,genero,usuario,password,especialidad,telefono)
