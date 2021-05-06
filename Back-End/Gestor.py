02e392a94f65f8e70e34c2d296fac52b913da59f
Bryan <2780289260101@ingenieria.usac.edu.gt>
2021-05-06 15:36:10 -0600

    ordenamiento

--- Back-End/Gestor.py
+++ Back-End/Gestor.py
@@ -390,3 +390,39 @@ class Gestor:
             if i.usuario == usuario:
                 return False
         return True
+
+    #Ordenamiento
+    def ordenamiento_por_mezcla(self,lista):
+        if len(lista) > 1:
+            medio = len(lista) // 2
+            izquierda = lista[:medio]
+            derecha = lista[medio:]
+            print(izquierda, '-' * 5,derecha)
+            #llamada recursiva en cada mitad
+            ordenamiento_por_mezcla(izquierda)
+            ordenamiento_por_mezcla(derecha)
+            #iteradores para recorrer las dos sublistas
+            i = 0
+            j = 0
+            #iterador para la lista principal
+            k = 0
+            while i < len(izquierda) and j < len(derecha):
+                if izquierda[i] < derecha[j]:
+                    lista[k] = izquierda[i]
+                    i += 1
+                else:
+                    lista[k] = derecha[j]
+                    j += 1
+                k += 1
+            while i < len(izquierda):
+                lista[k] = izquierda[i]
+                i += 1
+                k += 1
+            while j < len(derecha):
+                lista[k] = derecha[j]
+                j += 1
+                k += 1
+            print(f'izquierda {izquierda}, derecha {derecha}')
+            print(lista)
+            print('-' * 50)
+        return lista
